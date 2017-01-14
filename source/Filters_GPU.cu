02cdb5f0277cdf9a5bcbcb534ca6a0d57364af81
boris <bor1996@mail.ru>
2017-01-14 22:42:47 +0400

    Added something

--- source/Filters_GPU.cu
+++ source/Filters_GPU.cu
@@ -16,11 +16,12 @@ __global__ void compute(
 		unsigned int ver_radius,
 		float* ker)
 {
-	__shared__ float prod_cache[1024*3];
+	__shared__ float prod_cache[17*17*3];
+	__shared__ float cache2[17*3];
 
 	unsigned int img_cols = res_cols+2*hor_radius;
 	unsigned int hor_size = 2*hor_radius + 1;
-	unsigned int i;
+	unsigned int i, j;
 	float pix_res = 0.0;
 
 	prod_cache[(threadIdx.x*hor_size + threadIdx.y)*3 + threadIdx.z] =
@@ -29,10 +30,21 @@ __global__ void compute(
 
 	__syncthreads();
 
-	// FIXME
+	if (threadIdx.y == 0){
+		cache2[threadIdx.x*3 + threadIdx.z] = 0.0;
+		for (j = threadIdx.x*hor_size*3 + threadIdx.z; j < (threadIdx.x+1)*hor_size*3; j+=3)
+			cache2[threadIdx.x*3 + threadIdx.z] += prod_cache[j];
+	}
+
+	__syncthreads();
+
+	if (threadIdx.x == 0 && threadIdx.y == 0)
+		for (i = threadIdx.z; i < hor_size*3; i+=3)
+			pix_res += cache2[i];
+
+	__syncthreads();
+
 	if (threadIdx.x == 0 && threadIdx.y == 0){
-		for (i = threadIdx.z; i < hor_size*hor_size*3; i+=3)
-			pix_res += prod_cache[i];
 		res[(blockIdx.x*res_cols + blockIdx.y)*3 + threadIdx.z] = (unsigned char) pix_res;
 	}
 
