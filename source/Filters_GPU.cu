ea2520afb14477cc016e7475bb684737b7782a94
boris <bor1996@mail.ru>
2017-01-15 17:37:50 +0400

    Refactoring

--- source/Filters_GPU.cu
+++ source/Filters_GPU.cu
@@ -16,39 +16,36 @@ __global__ void compute(
 		unsigned int ver_radius,
 		float* ker)
 {
-	__shared__ float prod_cache[17*17*3];
-	__shared__ float cache2[17*3];
+	// 17 is maximum filter size
+	__shared__ float cache1[17*17*3];	// cache for every product
+	__shared__ float cache2[17*3];		// cache for row-reduce in box filter
 
 	unsigned int img_cols = res_cols+2*hor_radius;
 	unsigned int hor_size = 2*hor_radius + 1;
 	unsigned int i, j;
 	float pix_res = 0.0;
 
-	prod_cache[(threadIdx.x*hor_size + threadIdx.y)*3 + threadIdx.z] =
+	cache1[(threadIdx.x*hor_size + threadIdx.y)*3 + threadIdx.z] =
 			img[((blockIdx.x + threadIdx.x)*img_cols + blockIdx.y + threadIdx.y)*3
 			    + threadIdx.z] * ker[threadIdx.x*hor_size + threadIdx.y];
 
 	__syncthreads();
 
+	// reduce in each row
 	if (threadIdx.y == 0){
 		cache2[threadIdx.x*3 + threadIdx.z] = 0.0;
 		for (j = threadIdx.x*hor_size*3 + threadIdx.z; j < (threadIdx.x+1)*hor_size*3; j+=3)
-			cache2[threadIdx.x*3 + threadIdx.z] += prod_cache[j];
+			cache2[threadIdx.x*3 + threadIdx.z] += cache1[j];
 	}
 
 	__syncthreads();
 
-	if (threadIdx.x == 0 && threadIdx.y == 0)
+	// reduce on rows
+	if (threadIdx.x == 0 && threadIdx.y == 0){
 		for (i = threadIdx.z; i < hor_size*3; i+=3)
 			pix_res += cache2[i];
 
-	__syncthreads();
-
-	if (threadIdx.x == 0 && threadIdx.y == 0){
+		// save result
 		res[(blockIdx.x*res_cols + blockIdx.y)*3 + threadIdx.z] = (unsigned char) pix_res;
 	}
-
-	//res[(blockIdx.x*res_cols + blockIdx.y)*3 + threadIdx.z] =
-	//	img[((blockIdx.x + threadIdx.x)*img_cols + blockIdx.y + threadIdx.y)*3 + threadIdx.z]
-	//		*ker[threadIdx.x*hor_size + threadIdx.y];
 }
